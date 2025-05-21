8f4aba2c1dcf552d7249446a37a4242e4156982e
gonza <gonza@192.168.1.244>
2025-05-21 03:04:35 +0200

    a

--- src/educacion/educacion.java
+++ src/educacion/educacion.java
@@ -1,5 +1,15 @@
 package educacion;
 
+import java.util.Scanner;
+
 public class educacion {
+	public static void main(String[] args){
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("holllaaaasadsaddasdasdaaaaaaaaaa");
+		int numero=0;
+		System.out.println("si es primo me la comes");
+		numero= teclado.nextInt();
+	}
 
 }
